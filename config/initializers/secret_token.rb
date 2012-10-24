4732fceed94bfd6c1feefe56068a048af89a5943
login <login@vagrant.(none)>
2012-10-24 00:47:25 -0400

    initial imports

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simple::Application.config.secret_token = '3f5fafc627f5c1798dcde953fdb13035917bc7d08cdd5f5b973c546812ec2334c840cc7dab6485e384230c2738114f1e012f39329df834d0597cb04f9a1a9136'
